7fee529645a1bfd67035edd8122810b298552669
Daniel Toro <Daniel.ToroVargas@HS-Augsburg.DE>
2022-08-06 12:29:12 +0200

    app name update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyB_hifCjvJb7jf3hnZozfzlnJQ6N0iYuXI",
-    authDomain: "ionic5-firebase-authentication.firebaseapp.com",
-    databaseURL: "https://ionic5-firebase-authentication.firebaseio.com",
-    projectId: "ionic5-firebase-authentication",
-  }
+    authDomain: "graph-friends.firebaseapp.com",
+    databaseURL: "https://graph-friends.firebaseio.com",
+    projectId: "graph-friends",
+  },
 };
